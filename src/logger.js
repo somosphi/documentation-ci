5b0ab996edfe4beb2da34017721215842127e548
Henrique Schmidt <henrique.rieger@gmail.com>
2020-03-16 09:58:22 -0300

    remove duplicate logger

--- src/logger.js
+++ src/logger.js
@@ -5,17 +5,10 @@ const env = require('./env');
 const stdout = new PrettyStream();
 stdout.pipe(process.stdout);
 
-const stderr = new PrettyStream();
-stderr.pipe(process.stderr);
-
 const logger = bunyan.createLogger({
   name: env.NAME,
   streams: [
     {
-      stream: stdout,
-    },
-    {
-      level: 'error',
       stream: stdout,
     },
   ],
